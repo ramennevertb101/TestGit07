2f9bbcbf0acf7e259ad1bbd404598d1a19853e07
Thavaree Sampaothong <thavaree.samp@gmail.com>
2023-12-04 16:18:43 +0700

    Second try - change wording

---
+++ InClass/app.py
@@ -0,0 +1,21 @@
+def test01():
+    a = 5
+    #print(a)
+
+def test02():
+    c1 = 3
+    c2 = 3
+    code = str(c1) + str(c2)
+    print(code)
+    #string combine
+
+#print("A")
+
+def test03():
+    s1 = '10'
+    s2 = '21'
+    sum = int(s1) + int(s2)
+    print(sum)
+    #integer combine
+
+
---
+++ InClass/main.py
@@ -0,0 +1,6 @@
+import app
+
+if __name__ == '__main__':
+    app.test01()
+    app.test02()
+    app.test03()
